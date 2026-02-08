ed814d0f491fc57d01769fa26b73915e05e0f955
santarsierilorenzo <lorenzo.santarsieri@outlook.com>
2026-02-08 13:19:23 +0000

    math fix

--- quant_kit/returns.py
+++ quant_kit/returns.py
@@ -44,24 +44,26 @@ def cum_returns(
         Cumulative performance series. The output type matches the input
         type when possible.
 
+
+        
     Notes
     -----
-    - Missing values (NaNs) in the input series are treated as zeros prior to
+    For simple returns, cumulative performance is computed using multiplicative
+    compounding:
+
+    .. math::
+
+    R_t = \prod_{i=1}^{t} (1 + r_i) - 1
+
+    For log-returns and PnL values, cumulative performance is computed using
+    an additive aggregation:
+
+    .. math::
+
+    R_t = \sum_{i=1}^{t} r_i
+
+    Missing values (NaNs) in the input series are treated as zeros prior to
     aggregation.
-
-    - For simple returns, cumulative performance is computed using
-    multiplicative compounding:
-
-        .. math::
-
-        R_t = \\prod_{i=1}^{t} (1 + r_i) - 1
-
-        For log-returns and PnL values, cumulative performance is computed
-        using an additive aggregation:
-
-        .. math::
-
-        R_t = \\sum_{i=1}^{t} r_i
     """
 
     if kind not in {"simple", "pnl", "log"}:
