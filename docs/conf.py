d4c31835a25a0f390f9017f19c133b7587998587
santarsierilorenzo <lorenzo.santarsieri@outlook.com>
2026-02-08 14:30:14 +0000

    furo

--- docs/conf.py
+++ docs/conf.py
@@ -32,13 +32,10 @@ exclude_patterns: List[str] = [
 
 source_suffix = ".rst"
 
-html_theme = "sphinx_book_theme"
+html_theme = "furo"
 
 html_theme_options = {
-    "repository_url": "",
-    "use_edit_page_button": False,
-    "use_repository_button": False,
-    "home_page_in_toc": True,
+    "navigation_with_keys": True,
 }
 
 html_static_path = ["_static"]
